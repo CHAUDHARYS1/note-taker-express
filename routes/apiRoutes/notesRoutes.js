3081cc1dc0c25476c53cfe101147e00e0f0461f9
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-26 10:23:33 -0500

    able to add single or multiple notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,28 +1,39 @@
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
-const { notes } = require('../../data/db');
+const fs = require('fs');
+const path = require('path');
+
+const {
+    createNewNote,
+    validateNote
+} = require('../../lib/notes');
+const notes = require('../../data/db');
 
 // GET /api/notes
 router.get('/notes', (req, res) => {
-    res.json(notes);
+    fs.readFile(
+        path.join(__dirname, '../../data/db.json'), (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            const notes = JSON.parse(data);
+            res.json(notes);
+        });
+
     console.log(notes);
 });
 
 // POST /api/notes
 router.post('/notes', (req, res) => {
 
-    req.body.id = notes.length.toString();    
-
-    if(!validateNote(req.body)) {
+    req.body.id = notes.length.toString();
+    console.log(req.body);
+    if (!validateNote(req.body)) {
         res.status(400).send('This note is not properly formated.');
-    } else{
-        const note = createNewNote(req.body, notes);
-        res.json(note);
+    } else {
+        createNewNote(req.body).then(note => res.json(note));
     }
 });
 
